e76cff79d989cab2d3613f5990bbdb84e6608318
Brandon Chui <bbchui325@gmail.com>
2017-09-26 12:53:20 -0700

    testing styles: flex box and colors

--- app/components/Splash.js
+++ app/components/Splash.js
@@ -2,11 +2,35 @@ import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
 export default class Splash extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {name: "Brandon Chui"}
+  }
+
   render() {
     return(
-      <View>
-        <Text>{this.props.name}</Text>
+      <View style={styles.myView}>
+        <Text style={styles.myText}>{this.state.name}</Text>
+        <Text style={styles.myText}>Box 2</Text>
+        <Text style={styles.myText}>Box 3</Text>
       </View>
     );
   }
+
+
+
 }
+
+
+const styles = StyleSheet.create({
+  myText: {
+    color:'red',
+    backgroundColor:'black',
+    padding: 10
+  },
+
+  myView: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+})
--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,59 @@
-import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 import Splash from './app/components/Splash.js';
 
-export default class myapp extends Component{
+
+export default class myapp extends Component {
   render() {
-    return(
-      <View>
-        <Splash name="Brandon"/>
+    return (
+      <View style={styles.container}>
+        <View>
+          <Splash name="Brandon"/>
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
 AppRegistry.registerComponent('myapp', () => myapp);
+
+// <Text style={styles.welcome}>
+//   Welcome to React Native!
+// </Text>
+// <Text style={styles.instructions}>
+//   To get started, edit index.ios.js
+// </Text>
+// <Text style={styles.instructions}>
+//   Press Cmd+R to reload,{'\n'}
+//   Cmd+D or shake for dev menu
+// </Text>
